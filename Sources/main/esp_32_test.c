8d275ff8cfe7e83082e27d09d7b72ed773b66bc9
Jack Lukomski <jtlukomski@gmail.com>
2023-05-18 13:57:33 -0400

    built the project so it will not take as long in the future

--- Sources/main/esp_32_test.c
+++ Sources/main/esp_32_test.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-void app_main(void)
-{
+#define LED_PIN GPIO_NUM_13
+#define DELAY_MS 1000
 
+void app_main() {
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
+
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
+    }
 }
